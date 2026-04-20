b0ab5bfdc677b587011676f0b54b977ab66b0a9f
Charles <charlesngatia.cn@gmail.com>
2026-04-20 17:32:08 +0300

    ..

--- server/app.py
+++ server/app.py
@@ -2,28 +2,22 @@ from flask import Flask, make_response
 
 app = Flask(__name__)
 
-customers = ["bob", "john"]
-
-contracts = {
+contract_information = {
     1: "This contract is for John and building a shed",
     2: "This contract is for Bob and painting a house"
 }
 
-@app.route("/")
-def index():
-    return "Welcome to Contracts API"
+customer_information = ["bob", "john"]
 
-
-@app.route("/contract/<int:contract_id>")
-def get_contract(contract_id):
-    if contract_id in contracts:
-        return contracts[contract_id], 200
+@app.route("/contract/<int:id>")
+def contract(id):
+    if id in contract_information:
+        return contract_information[id], 200
     return make_response("Contract not found", 404)
 
-
-@app.route("/customer/<string:name>")
-def get_customer(customer_name):
-    if customer_name.lower() in customers:
+@app.route("/customer/<string:customer_name>")
+def customer(customer_name):
+    if customer_name.lower() in customer_information:
         return "", 204
     return make_response("Customer not found", 404)
 
